72e86613f0a22381eefd85b1c3434c6c119b5c60
emontecalvo <emilymontecalvo@gmail.com>
2017-11-07 10:36:42 -0800

    updated MoveTowards function

--- Assets/MOB.cs
+++ Assets/MOB.cs
@@ -8,49 +8,70 @@ public class MOB : MonoBehaviour {
 	public Vector3 AStart;
 	public Vector3 BEnd;
 	public bool One = false;
+	public float MySpeed = 1f;
 
-	// Use this for initialization
+
 	void Start () {
 		transform.position = AStart;
 
 		Debug.LogFormat ("Hi world" + DateTime.Now.ToString ());
-		MoveMob (1, 10, 1);
-		MoveMob (1, -5, 1);
-		MoveMob (-1, 5, 2);
+//		MoveTowards (1, 10, 1);
+//		MoveTowards (1, -5, 1);
+//		MoveTowards (-1, 5, 2);
 	}
 
 	void OnDrawGizmos () {
-	
 		Gizmos.DrawLine (AStart, BEnd);
-	
 	}
 		
-
-	// Update is called once per frame
 	void Update () {
-//		Vector3 delta = new Vector3 (AStart, BEnd, 0);
-//		transform.position = transform.position + delta;
-//		if (!One) {
-//			StartCoroutine(Move_Routine(this.transform, AStart, BEnd));
-//			One = true;
-//		}
-
+		Vector3 Position = transform.position;
+//		float P1 = MoveTowards (Position.x, BEnd.x, MySpeed * Time.deltaTime);
+//		float P2 = MoveTowards (Position.y, BEnd.y, MySpeed * Time.deltaTime);
+//		float P3 = MoveTowards (Position.z, BEnd.z, MySpeed * Time.deltaTime);
+		float P1 = MoveTowards2 (Position.x, BEnd.x, MySpeed * Time.deltaTime);
+		float P2 = MoveTowards2 (Position.y, BEnd.y, MySpeed * Time.deltaTime);
+		float P3 = MoveTowards2 (Position.z, BEnd.z, MySpeed * Time.deltaTime);
+		Position.x = P1;
+		Position.y = P2;
+		Position.z = P3;
+		transform.position = Position;
 	}
 
-	private IEnumerator Move_Routine(Transform transform, Vector3 from, Vector3 to)
-	{
-		float t = 0f;
-		while(t < 1f)
-		{
-			t += Time.deltaTime;
-			transform.position = Vector3.Lerp(from, to, Mathf.SmoothStep(0f, 1f, t));
-			yield return null;
-		}
-	}
-
-
-	float MoveMob (float currentPosition, float targetPosition, float maxDistance) {
+	float MoveTowards (float currentPosition, float targetPosition, float maxDistance) {
 		Debug.Log ("MoveMob called");
+		Debug.Log ("positions: " + currentPosition + " " + targetPosition + " " + maxDistance);
+		if (currentPosition < targetPosition) {
+			if (currentPosition <= targetPosition) {
+				currentPosition += maxDistance;
+			} else {
+				currentPosition = targetPosition;
+			}
+		} else if (currentPosition > targetPosition) {
+			if ((currentPosition - maxDistance) >= targetPosition) {
+				currentPosition -= maxDistance;
+			} else {
+				currentPosition = targetPosition;
+			}
+		}
+		return currentPosition;
+	}
 
+	float MoveTowards2 (float currentPosition, float targetPosition, float maxDistance) {
+		Debug.Log ("MoveMob2 called");
+		Debug.Log ("positions: " + currentPosition + " " + targetPosition + " " + maxDistance);
+
+		float delta = targetPosition - currentPosition;
+
+		if (delta > maxDistance) {
+			delta *= maxDistance;
+		}
+
+		if (delta < -maxDistance) {
+			delta *= -1;
+		}
+
+		float CP2 = currentPosition + delta;
+		return CP2;
 	}
 }
